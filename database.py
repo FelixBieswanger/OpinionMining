ccdf1dbf129af297fe110550d330ac38bc24d81f
FelixBieswanger <felix.bieswanger@googlemail.com>
2020-03-21 11:15:05 +0100

    Update for CloudDB

--- database.py
+++ database.py
@@ -1,20 +1,23 @@
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+import getpass
 
 class Database:
 
     def __init__(self, logger=None):
         #connecting to mongodb running on default host and port
-        client = MongoClient('mongodb://localhost:27017/')
+        user = getpass.getuser()
+        pw = user+"!"
+        client = MongoClient("mongodb+srv://"+user+":"+pw+"@cluster0-kzqxg.mongodb.net/data")
         self.db = db = client.data
 
         self.logger = logger
 
-    def insert_article(self, data):
+    def insert_data(self,collection,data):
         #check if article has already bin insertet by date,title,author
-        if self.db.article.count_documents({"article_url": data["article_url"]}):
+        if self.db[collection].count_documents({"article_url": data["article_url"]}):
             self.logger.warn("Article already in db")
-            return
+            return "already"
         
         #insert article information into db
         try:
@@ -23,8 +26,8 @@ class Database:
         except:
             self.logger.critical("Article could not be stored")
             
-    def get_all(self):
-        return self.db.article.find()
+    def get_all(self,collection):
+        return [doc for doc in self.db[collection].find()]
 
 
     def update_article(self,data):
@@ -35,13 +38,21 @@ class Database:
             pass
 
 
-    def find_by_id(self, id):
+    def find_by_id(self,collection, id):
         try:
-            return self.db.article.find_one({"_id": ObjectId(id)})
+            return self.db[collection].find_one({"_id": ObjectId(id)})
         except:
-            print("Post with ID could not be found")
-            return
+            return None
         
+
+    def getNewDoc(self):
+        docs = self.db.article.find()
+        trigger = True
+        for i in range(docs.count()):
+            found = self.find_by_id(id= docs[i]["_id"], collection="stich")
+            if found is None:
+                return docs[i]
+
 
 
 
