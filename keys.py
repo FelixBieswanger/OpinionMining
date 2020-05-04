4adf21837304d2d473e1f7ed69e2eeb8bfed220f
FelixBieswanger <felix.bieswanger@googlemail.com>
2020-05-04 16:21:47 +0200

    make path relative

--- keys.py
+++ keys.py
@@ -1,7 +1,7 @@
 import json
 
 def get_key(name):
-    with open("/Users/felixbieswanger/Desktop/Uni_Stuff/Bachelorarbeit/OpionionMining/resources/keys.json") as file:
+    with open("resources/keys.json") as file:
         keys = json.loads(file.read())
         try:
             return keys[name]
