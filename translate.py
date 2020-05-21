3ea5b291fa4fb9b868f79fc2d48592b06f0f5b9f
FelixBieswanger <felix.bieswanger@googlemail.com>
2020-05-21 18:58:32 +0200

    impl char limit bug fix

--- translate.py
+++ translate.py
@@ -4,23 +4,44 @@ import preprocessing
 
 
 db = Database()
-trans = Translator(service_urls=["translate.google.com"])
 
 german_articles = db.get_querry(collection="article", querry={
     "$or": [{"source": "sueddeutsche"}, {"source": "zeit"}]})
 
-for article in german_articles:
+for article in german_articles[530:]:
     if "text-de" not in article:
-        #save original text
-        article["text-de"] = article["text"]
+        trans = Translator(service_urls=["translate.google.com"])
+        try:
+            #save original text
+            article["text-de"] = article["text"]
+            clean_text = preprocessing.clean_string(article["text"])
 
-        clean_text = preprocessing.clean_string(article["text"])
-        trans_text = trans.translate(clean_text,dest="en").text
+            translated_text = ""
+            rest = clean_text
+            while len(rest) != 0:
+                if len(rest) > 9999:
+                    cutoff = rest[:9999].rfind(".")
+                    part = rest[:cutoff]
+                    rest = rest[cutoff:]
+                else:
+                    part = rest
+                    rest = []
+            
+                translated_text += trans.translate(part, dest="en").text
 
-        article["text"] = trans_text
-        
-        db.update_article(collection="article",data=article)
+            article["text"] = translated_text
+            db.update_article(collection="article",data=article)
 
-    print(german_articles.index(article),"of",len(german_articles),"translated")
+
+            print(german_articles.index(article), "of",
+                  len(german_articles), "translated")
+        except Exception as e:
+            print(e.with_traceback())
+            print("index", german_articles.index(article))
+            break
+    else:
+        print("Already Translated")
+
+
 
 
