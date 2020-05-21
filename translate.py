e7499378d65059d7875a969ff065f6900ec933bf
FelixBieswanger <felix.bieswanger@googlemail.com>
2020-05-21 20:26:24 +0200

    add est time

--- translate.py
+++ translate.py
@@ -1,6 +1,7 @@
 from google.cloud import translate_v2
 from database import Database
 import preprocessing
+import time
 
 db = Database()
 translate_client = translate_v2.Client()
@@ -9,6 +10,9 @@ german_articles = db.get_querry(collection="article", querry={
     "$or": [{"source": "sueddeutsche"}, {"source": "zeit"}]})
 
 for article in german_articles[:10]:
+
+    start = time.time()
+
     if "text-de" in article:
         #case falls bereits übersetzt wurde
         raw_text = article["text-de"]
@@ -26,7 +30,23 @@ for article in german_articles[:10]:
         article["text"] = translated_text
         db.update_article(collection="article", data=article)
 
-        print("Translated ", german_articles.index(article)+1,"of",len(german_articles))
+
+        time.sleep(0.5)
+
+        end = time.time()
+        #sec
+        duration = end - start
+    
+        remaining_time = (len(german_articles)-german_articles.index(article))*duration
+        #min
+        remaining_time = int(remaining_time/60)
+
+
+        print("Translated ", german_articles.index(
+            article)+1, "of", len(german_articles), "| est. remaining duration",remaining_time,"min")
+
+        
+
     except Exception as e:
         print(e.with_traceback())
         print("Fail Index",german_articles.index(article))
