4a5f21294fb063c5fe86e4e766d7e32f4d5aca73
FelixBieswanger <felix.bieswanger@googlemail.com>
2020-06-01 14:00:47 +0200

    minor changes

--- translate.py
+++ translate.py
@@ -5,6 +5,9 @@ import preprocessing
 import time
 from urllib.error import HTTPError
 import traceback
+import os
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./resources/opinionmininghdm-f5403fd25fa4.json"
 
 db = Database()
 
@@ -12,7 +15,7 @@ db = Database()
 german_articles = db.get_querry(collection="article", querry={
     "$or": [{"source": "sueddeutsche"}, {"source": "zeit"}]})
 
-index = 186
+index = 755
 old_index = 0
 
 while index<len(german_articles):
@@ -25,46 +28,43 @@ while index<len(german_articles):
     translate_client = translate_v2.Client()
     article = german_articles[index]
 
-    
-
-    if "text-de" in article:
-        #case falls bereits übersetzt wurde
-        raw_text = article["text-de"]
-    else:
-        #case falls noch nicht übersetzt wurde
-        #orginaltext in text-de abspeichern
+    if "text-de" not in article:
+       
         raw_text = article["text"]
         article["text-de"] = raw_text
 
-    clean_text = preprocessing.clean_string(raw_text)
+        clean_text = preprocessing.clean_string(raw_text)
 
-    try:
         try:
-            result = translate_client.translate(clean_text,target_language="en")
-        except (Forbidden,ServiceUnavailable):
-            print("Wait because of rate limit")
-            time.sleep(10)
-            continue
+            try:
+                result = translate_client.translate(clean_text,target_language="en")
+            except (Forbidden,ServiceUnavailable):
+                print("Wait because of rate limit")
+                time.sleep(10)
+                continue
 
-        translated_text = result["translatedText"]
-        article["text"] = translated_text
-        db.update_article(collection="article", data=article)
-        end = time.time()
-        #sec
-        duration = end - start
-    
-        remaining_time = (len(german_articles)-german_articles.index(article))*duration
-        #min
-        remaining_time = int(remaining_time/60)
+            translated_text = result["translatedText"]
+            article["text"] = translated_text
+            db.update_article(collection="article", data=article)
+            end = time.time()
+            #sec
+            duration = end - start
+        
+            remaining_time = (len(german_articles)-german_articles.index(article))*duration
+            #min
+            remaining_time = int(remaining_time/60)
 
-        print("Translated ", german_articles.index(
-                article)+1, "of", len(german_articles), "| est. remaining duration", remaining_time, "min")
+            print("Translated ", german_articles.index(
+                    article)+1, "of", len(german_articles), "| est. remaining duration", remaining_time, "min")
 
+            index+=1
+
+        except Exception as e:
+            traceback.print_exc()
+            print("Fail Index",german_articles.index(article))
+            break
+    else:
         index+=1
-
-    except Exception as e:
-        traceback.print_exc()
-        print("Fail Index",german_articles.index(article))
-        break
-    
-     
+        print("already translated")
+        
+        
