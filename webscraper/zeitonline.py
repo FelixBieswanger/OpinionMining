3f3e9f55acba15df384e0472318d9868acfb74bb
FelixBieswanger <felix.bieswanger@googlemail.com>
2020-06-25 15:29:12 +0200

    bug fix

--- webscraper/zeitonline.py
+++ webscraper/zeitonline.py
@@ -23,38 +23,42 @@ for i in range(277):
     }
     # create Session to login and send login information with every further request
     with requests.Session() as s:
-        p = s.post('https://meine.zeit.de/anmelden', data=payload)
+        try:
+            p = s.post('https://meine.zeit.de/anmelden', data=payload)
 
-        # Check if login worked
-        soup = BeautifulSoup(p.content, "html.parser")
-        name = soup.find("span", {"class": "nav__user-name"}).text
+            # Check if login worked
+            soup = BeautifulSoup(p.content, "html.parser")
+            name = soup.find("span", {"class": "nav__user-name"}).text
 
-        articles = soup.findAll("article", {"class": "zon-teaser-standard"})
+            articles = soup.findAll("article", {"class": "zon-teaser-standard"})
 
-        for article in articles:
-            try:
-                url = article.find(
-                    "a", {"class", "zon-teaser-standard__combined-link"})["href"]
+            for article in articles:
+                try:
+                    url = article.find(
+                        "a", {"class", "zon-teaser-standard__combined-link"})["href"]
 
-                page = BeautifulSoup(s.get(url).content, "html.parser")
+                    page = BeautifulSoup(s.get(url).content, "html.parser")
 
-                store = dict()
-                store["headline"] = page.find(
-                    "span", {"class": "article-heading__title"}).text
-                store["section"] = page.find(
-                    "span", {"class": "article-heading__kicker"}).text
-                store["date"] = page.find("time", {"class": "metadata__date"})["datetime"]
-                store["article_url"] = url
-                store["search-term"] = search_term
-                store["source"] = "zeit"
+                    store = dict()
+                    store["headline"] = page.find(
+                        "span", {"class": "article-heading__title"}).text
+                    store["section"] = page.find(
+                        "span", {"class": "article-heading__kicker"}).text
+                    store["date"] = page.find("time", {"class": "metadata__date"})["datetime"]
+                    store["article_url"] = url
+                    store["search-term"] = search_term
+                    store["source"] = "zeit"
 
-                text = ""
-                for paragraph in page.find_all("p", {"class": "paragraph article__item"}):
-                    text += paragraph.text
-                store["text"] = text
-                
-                if text != "":
-                    db.insert_data(collection="article",data=store)
-            except Exception as e:
-                logger.warning(e)
-                pass
+                    text = ""
+                    for paragraph in page.find_all("p", {"class": "paragraph article__item"}):
+                        text += paragraph.text
+                    store["text"] = text
+                    
+                    if text != "":
+                        db.insert_data(collection="article",data=store)
+                except Exception as e:
+                    logger.warning(e)
+                    pass
+        except:
+            pass
+        
