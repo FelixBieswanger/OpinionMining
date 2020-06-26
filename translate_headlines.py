829d381306e55d3053aa19bbcd1ff3f7c2fb64fe
FelixBieswanger <felix.bieswanger@googlemail.com>
2020-06-26 15:49:40 +0200

    update

---
+++ translate_headlines.py
@@ -0,0 +1,62 @@
+from google.cloud import translate_v2
+from google.api_core.exceptions import Forbidden,ServiceUnavailable
+from database import Database
+import preprocessing
+import time
+from urllib.error import HTTPError
+import traceback
+import os
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./resources/google_cloud_api.json"
+
+db = Database()
+
+
+german_articles = db.get_querry(collection="date", querry={"language": "de"})
+
+index = 0
+old_index = 0
+
+while index<len(german_articles):
+
+    if index != old_index:
+        start = time.time()
+
+    old_index = index
+
+    translate_client = translate_v2.Client()
+    article = german_articles[index]
+
+    if "headline-de" not in article:
+       
+        raw_text = article["headline"]
+        article["headline-de"] = raw_text
+
+        clean_text = preprocessing.clean_string(raw_text)
+
+        try:
+            try:
+                result = translate_client.translate(clean_text,target_language="en")
+            except (Forbidden,ServiceUnavailable):
+                print("Wait because of rate limit")
+                time.sleep(10)
+                continue
+
+            translated_text = result["translatedText"]
+            article["headline"] = translated_text
+            db.update_article(collection="date", data=article)
+            
+            print("Translated ", german_articles.index(
+                    article)+1, "of", len(german_articles))
+
+            index+=1
+
+        except Exception as e:
+            traceback.print_exc()
+            print("Fail Index",german_articles.index(article))
+            break
+    else:
+        index+=1
+        print("already translated or to big!")
+        
+        
